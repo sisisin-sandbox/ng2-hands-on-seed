b16467cf2479340a7e090c2d68acdf239e97effa
laco0416 <laco0416+github@gmail.com>
2016-04-05 00:22:04 +0900

    4/10用資料追加とシードプロジェクトの修正

--- config.js
+++ config.js
@@ -1,8 +1,16 @@
 System.config({
-    packages: {        
-      app: {
-        format: 'register',
-        defaultExtension: 'js'
-      }
+  transpiler: 'typescript',
+  typescriptOptions: {
+    emitDecoratorMetadata: true
+  },
+  map: {
+    app: 'app',
+    typescript: 'node_modules/typescript/lib/typescript.js',
+  },
+  packages: {
+    app: {
+      defaultExtension: 'ts',
+      main: 'main.ts',
     }
-  });
+  }
+});
