2a9c0a2e03e1f27f70dd1c49114acbe13420ca2e
sisisin <azsisisin@gmail.com>
2016-07-24 16:17:47 +0900

    use browserify

--- courses/quickstart/app/main.ts
+++ courses/quickstart/app/main.ts
@@ -1,3 +1,7 @@
+import 'core-js';
+import 'rxjs/Rx';
+import 'zone.js/dist/zone';
+
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {AppComponent} from './app.component';
 import {appRouterProviders} from './app.routes';
