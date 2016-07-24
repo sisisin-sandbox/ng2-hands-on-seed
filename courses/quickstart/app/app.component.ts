dc2bd58b2c03ae5a3c3ae048591fce4a2889788d
sisisin <azsisisin@gmail.com>
2016-07-24 13:39:44 +0900

    2-3まで

--- courses/quickstart/app/app.component.ts
+++ courses/quickstart/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core'
 import {Hero} from './hero';
+import {HeroDetailComponent} from './hero-detail.component';
 
 const HEROES: Hero[] = [
   { "id": 11, "name": "Mr. Nice" },
@@ -24,15 +25,10 @@ const HEROES: Hero[] = [
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
     </ul>
-    <div *ngIf="selectedHero">
-      <h2>{{selectedHero.name}} details!</h2>
-      <div><label>id: </label>{{selectedHero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input [(ngModel)]="selectedHero.name" placeholder="name" />
-      </div>
-    </div>
-  ` })
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `
+  , directives: [HeroDetailComponent]
+})
 export class AppComponent {
   title = 'Tour of Heros';
   heros = HEROES;
--- courses/quickstart/app/hero.ts
+++ courses/quickstart/app/hero.ts
@@ -1,1 +1,1 @@
-export interface Hero { id: number; name: string; }
+export class Hero { id: number; name: string; }
